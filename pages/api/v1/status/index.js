6c009297c16a38ca4dd684758e6fbe0bf0c5a1db
Diego Moreira <138245092+diegomoreiraiam@users.noreply.github.com>
2024-06-26 20:26:47 +0000

    finaliza endpoint de

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,36 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1+1 as SUM;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "Deu bom, acima da média" });
+  const updateAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionResult = await database.query(
+    "SHOW max_connections",
+  );
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+
+
+  const databaseOpenedConnectionResult = await database.query({ text: "SELECT COUNT (*)::int FROM pg_stat_activity WHERE datname = $1", values: [databaseName]}
+  );
+
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionResult.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updateAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        opened_connections: databaseOpenedConnectionsValue,
+      },
+    },
+  });
 }
 
 export default status;
